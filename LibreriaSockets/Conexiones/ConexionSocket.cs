f508587a21350d2020fce0d1cd1addce2f4fcb4d
leonardo wilches <lwilches@gmail.com>
2017-07-31 18:09:35 -0500

    sockets

---
+++ LibreriaSockets/Conexiones/ConexionSocket.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibreriaSockets.Conexiones
+{
+    public class ConexionSocket
+    {
+        private Socket _cliente;
+        private Task _tareaEntrada;
+        private CancellationToken _cancellationtoken;
+        private readonly List<string> _mensajesEntrada;
+        private readonly string _infoCliente;
+
+
+        private readonly Guid _identificador;
+
+
+        public string InfoCliente { get { return _infoCliente; } }
+        public Guid Id { get { return _identificador; } }
+
+        public Socket Cliente
+        {
+            get { return _cliente; }
+            set { _cliente = value; }
+        }
+
+        public Task TareaCanalEntrada { get { return _tareaEntrada; } }
+
+        public ConexionSocket(Socket socket, CancellationToken token)
+        {
+            _mensajesEntrada = new List<string>();
+            _cancellationtoken = token;
+            _cliente = socket;
+            _infoCliente = socket.RemoteEndPoint.ToString();
+            _identificador = new Guid();
+        }
+
+        public IEnumerable<string> GetMensajes()
+        {
+            foreach (var mensaje in _mensajesEntrada)
+            {
+                yield return mensaje;
+            }
+        }
+
+       
+
+        public void EscucharCanalEntrada()
+        {
+           
+            try
+            {
+
+                byte[] serverBuffer = new byte[10025];
+                int bytes = 0;
+                serverBuffer = new byte[10025];
+                bytes = 0;
+                bytes = _cliente.Receive(serverBuffer, serverBuffer.Length, 0);
+                _cancellationtoken.ThrowIfCancellationRequested();
+                var texto = Encoding.ASCII.GetString(serverBuffer, 0, bytes);
+                ///si recibio algo por le socket
+                if (!string.IsNullOrWhiteSpace(texto))
+                {
+                    ///TODO: HACER ALGO...............
+                    ///
+                    ///
+                    var file = new FileInfo("mensajes.txt");
+                    using (var stream = file.Open(FileMode.Append, FileAccess.Write))
+                    {
+                        using (var wr = new System.IO.StreamWriter(stream))
+                        {
+                            wr.WriteLine($"[{DateTime.Now}] -  Mensaje {texto} from { _infoCliente}");
+                        }
+                    }
+                }
+                Cliente.Close();
+            }
+            catch (Exception ex)
+            {
+                DesconectarCliente();
+            }
+
+        }
+
+        public void DesconectarCliente()
+        {
+            try
+            {
+             
+                _cliente.Close();
+                _cliente.Dispose();
+                _mensajesEntrada.Clear();
+            }
+            catch (Exception) { }
+        }
+
+        public void EnviarMensaje(string mensaje)
+        {
+            if (_cliente.Connected)
+            {
+                var serverBuffer = Encoding.ASCII.GetBytes(mensaje);
+                _cliente.Send(serverBuffer);
+
+            }
+        }
+
+    }
+
+}
---
+++ LibreriaSockets/Server/ServerSockets.cs
@@ -0,0 +1,93 @@
+﻿using LibreriaSockets.Conexiones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibreriaSockets
+{
+    public class Server
+    {
+        private TcpListener _listener;
+        private IPAddress _ip;
+        private CancellationTokenSource _tokenSource;
+        private static object objLock = new object();
+       
+
+        
+        public Server()
+        {
+            foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    _ip = ip;
+                    break;
+                }
+            }
+
+        }
+
+        public Server(CancellationTokenSource _tokenSource)
+            : this()
+        {
+            this._tokenSource = _tokenSource;
+        }
+
+      
+
+        public void IniciarServidor(int puerto)
+        {
+            try
+            {
+
+
+                var token = _tokenSource.Token;
+
+                _listener = new TcpListener(_ip, puerto);
+                _listener.Start();
+                while (true)
+                {
+
+                    var cliente = _listener.AcceptSocket();
+                    token.ThrowIfCancellationRequested();
+                    lock (objLock)
+                    {
+                        var conexion = new ConexionSocket(cliente, token);
+                        conexion.EscucharCanalEntrada();
+                        cliente.Close();
+                    }
+                }
+            }
+            catch (OperationCanceledException ex)
+            {
+                _listener.Stop();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public void DetenerServicio()
+        {
+            _tokenSource.Cancel();
+            _listener.Stop();
+        }
+
+        public Socket GetPeticion()
+        {
+
+            return _listener.AcceptSocket();
+        }
+
+        public string GetIp()
+        {
+            return _ip.ToString();
+        }
+
+    }
+}
